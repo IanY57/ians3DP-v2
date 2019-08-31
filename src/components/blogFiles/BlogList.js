cdeeb074da5e5c91c185025c252e20c808498e05
IanY57 <iylandel@gmail.com>
2019-09-01 02:37:52 +1000

    BlogList - title and description added

--- src/components/blogFiles/BlogList.js
+++ src/components/blogFiles/BlogList.js
@@ -1,13 +1,31 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-const BlogList = () => {
+import {
+  setRem,
+  setFont,
+  setFlex,
+  setColor,
+} from "../../styledComponents/styles"
+
+const BlogList = ({ className }) => {
   const { posts } = useStaticQuery(getPosts)
   return (
-    <div>
-      <ul>
+    <div className={className}>
+      <ul className="artical">
         {posts.edges.map(({ node }) => {
-          return <li key={node.id}>{node.title}</li>
+          return (
+            <div>
+              <li key={node.id}>
+                <AniLink fade to="/" className="title">
+                  <h2>{node.title}</h2>
+                </AniLink>
+                <p className="description">{node.description.description}</p>
+              </li>
+            </div>
+          )
         })}
       </ul>
     </div>
@@ -21,10 +39,32 @@ const getPosts = graphql`
         node {
           published(formatString: "Do MMMM YYYY")
           title
+          description {
+            description
+          }
         }
       }
     }
   }
 `
 
-export default BlogList
+export default styled(BlogList)`
+  ${setFont.headingFont}
+  .artical {
+    text-align: center;
+    margin: ${setRem(20)};
+    list-style-type: none;
+  }
+  .title {
+  }
+  .description {
+    ${setFont.mainFont};
+  }
+  a {
+    color: ${setColor.mainBlack};
+    text-decoration: none;
+    &:hover {
+      color: ${setColor.primaryColor};
+    }
+  }
+`
